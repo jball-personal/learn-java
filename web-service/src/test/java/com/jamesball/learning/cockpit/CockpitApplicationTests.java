0ae9fcff64229df0abc879d5da2e0c604d427356
jbwovn <james.ball@wovn.io>
2020-10-16 12:27:54 +0100

    Setup skeleton web application

---
+++ web-service/src/test/java/com/jamesball/learning/cockpit/CockpitApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jamesball.learning.cockpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CockpitApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+
+}
